13bd4e8019bfa2b81b574b6a3ba77f7263745e2e
Beryl Onyancha <berylonyancha@gmail>
2019-06-02 22:49:50 +0300

    Create submit function

--- week1/src/app/add-form/add-form.component.ts
+++ week1/src/app/add-form/add-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quotes';
 
 @Component({
   selector: 'app-add-form',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-form.component.css']
 })
 export class AddFormComponent implements OnInit {
+  newQuote= new Quote(4,"","");
+  @Output() addQuote=new EventEmitter<Quote>();
 
+  submitGoal(){
+    this.addQuote.emit(this.newQuote);
+  }
   constructor() { }
 
   ngOnInit() {
