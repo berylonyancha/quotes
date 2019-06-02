0a108c39514191d7b4c8b1d889fd4b7709662573
Beryl Onyancha <berylonyancha@gmail>
2019-06-03 00:19:24 +0300

    Add dates to the quote array and to the add new quote function

--- week1/src/app/quote/quote.component.ts
+++ week1/src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import{Quote} from '../quotes'
 
 @Component({
@@ -8,17 +8,24 @@ import{Quote} from '../quotes'
 })
 export class QuoteComponent implements OnInit {
 
+  @Input() quote:any[];
+
   quotes = [
-    new Quote(1, "Your grief path is yours alone, and no one else can walk it, and no one else can understand it", "Terri Irwin"),
+    new Quote(1, "Your grief path is yours alone, and no one else can walk it, and no one else can understand it", "Terri Irwin", new Date(2011,4,9)),
 
-    new Quote(2, "When you want to take control of your life and make the most of everything around you", "Ed Lester"),
+    new Quote(2, "When you want to take control of your life make the most of everything around you", "Ed Lester", new Date(2002,11,5)),
 
-    new Quote(3, "Its never that and if it is si ni life basi", "Beryl Onyancha")
+    new Quote(3, "Kaa mbali na life man", "Koffee", new Date(2019,1,21)),
+
+    new Quote(4, "Its never that serious and if it is si ni life basi", "Beryl Onyancha", new Date(2019,6,2))
+
+    
 
   ]
   addNewQuote(quote){
     let quoteLength = this.quotes.length;
     quote.id=quoteLength+1;
+    quote.publishDate = new Date(quote.publishDate)
     this.quotes.push(quote)
 
 }
